883e9515613567ae38b95e37b2c73780c717b4e7
David Lu <davidlu5010@gmail.com>
2023-12-09 20:49:25 -0800

    Increased data

--- pruning_vae.py
+++ pruning_vae.py
@@ -118,7 +118,7 @@ if __name__ == '__main__':
     logger, writer = init_logger(config)
 
     #5814
-    sprite, sprite_test = Sprites('dataset/lpc-dataset/train',26 ), Sprites('dataset/lpc-dataset/test', 522)
+    sprite, sprite_test = Sprites('dataset/lpc-dataset/train',1000 ), Sprites('dataset/lpc-dataset/test', 522)
     batch_size = 32
     
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
@@ -206,6 +206,9 @@ if __name__ == '__main__':
         #            iteration=iteration,
         #            logger=logger)
     # TODO: Setup VAE Trainer
-        trainer = Trainer(mb.model, sprite, sprite_test, loader, None, test_f, batch_size=25, epochs=1, learning_rate=0.002, device=device)
+        trainer = Trainer(mb.model, sprite, sprite_test, loader, None, test_f, batch_size=25, epochs=100, learning_rate=0.002, device=device)
         trainer.train_model()
     # TODO: Compare timing
+    
+    # Try to train and generate
+    
